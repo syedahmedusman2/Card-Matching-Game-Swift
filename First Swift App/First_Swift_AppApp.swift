f954e23240ed642cb89d1e0d40e06d8015bd63f8
Ahmed Usman <ahmedusmanpakistan@gmail.com>
2024-01-01 00:03:30 +0500

    Initial Commit

---
+++ First Swift App/First_Swift_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_Swift_AppApp.swift
+//  First Swift App
+//
+//  Created by User on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct First_Swift_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
